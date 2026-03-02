75f472e5061d5fe8e5320468ba3e6e29ce14fdcf
antonyrickg-dotcom <antonyrickg@gmail.com>
2026-03-02 16:59:38 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,7 @@
 function novaCarreira() {
-  const jogador = {
-    nome: "Novo Jogador",
-    overall: 70,
-    temporada: 1
-  };
-
-  localStorage.setItem("carreira", JSON.stringify(jogador));
-  alert("Nova carreira criada!");
+  alert("Nova carreira");
 }
 
 function carregarCarreira() {
-  const carreira = localStorage.getItem("carreira");
-
-  if (!carreira) {
-    alert("Nenhuma carreira salva!");
-    return;
-  }
-
-  const dados = JSON.parse(carreira);
-  alert("Carreira carregada!\nJogador: " + dados.nome);
+  alert("Carregar carreira");
 }
